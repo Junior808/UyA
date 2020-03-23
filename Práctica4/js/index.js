a2a55ea0657e1026d8640044fcff02f9a1da1856
lauracah <laurach2b1213@gmail.com>
2020-03-23 15:36:42 +0000

    Card, icons, padding

---
+++ Práctica4/js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.slider');
+    // var instances = M.Slider.init(elems, options);
+});
+
+
+$(document).ready(function () {
+    $('.carousel').carousel();
+});

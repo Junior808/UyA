92cc090a40e3f9d5ed1a0dfeeec297d18a0ed566
Damian Eduardo Dominguez <alu0101138183@ull.edu.es>
2020-03-29 15:44:15 +0100

    Hecho cambio de divisas

--- Práctica5/js/Apartado3.js
+++ Práctica5/js/Apartado3.js
@@ -1,21 +1,23 @@
+const equivalencia = { 'EUR': { 'JPY': '120.52', 'USD': '1.12' }, 'USD': { 'JPY': '107.92', 'EUR': '0.90' }, 'JPY': { 'EUR': '0.0083', 'USD': '0.0093' } };
 
-function moneda(){
-    this.nombre
-    this.tipoDeCambio
+const boton = document.querySelector(".boton-convertir");
 
-    this.convertir = function(monto){
-      conversionResultado = monto = this.tipoDeCambio
-      document.getElementById('resultadoDelCambio').innerHTML = conversionResultado
+boton.onclick = () => {
+
+    const divisa1 = document.querySelector(".divisa-1").value;
+    const divisa2 = document.querySelector(".divisa-2").value;
+    const monto = document.querySelector(".monto").value;
+
+    const texto = document.querySelector(".resultado");
+    const p = document.createElement("p");
+
+    if (divisa1 === divisa2) {
+        p.textContent = monto;
+    } else {
+        const resultado = monto * equivalencia[divisa1][divisa2];
+        p.textContent = Math.round((resultado + Number.EPSILON) * 100) / 100;
     }
+    texto.replaceChild(p, texto.childNodes[0]);
 }
 
-var yen = new moneda()
-    yen.nombre = "Yenes"
-    yen.tipoDeCambio = 99.5
-
-function convertirMoneda(){
-
-    var montoSeleccionado = document.getElementById('monto').value
-    yen.convertir(montoSeleccionado)
-}
 

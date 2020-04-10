8782dd70981f74e673490de936a1597f79894e21
barbaragd <alu0101108452@ull.edu.es>
2020-04-10 20:37:22 +0100

    Muestra el html bien

--- Práctica6/js/index.js
+++ Práctica6/js/index.js
@@ -1,19 +1,18 @@
 const xhttp = new XMLHttpRequest();
 
-$(document).ready(function () {
-    $(".boton").click(function () {
+$(document).ready(function() {
+    $(".boton").click(function() {
 
-        xhttp.onreadystatechange = function () {
+        xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 const response = JSON.parse(this.responseText);
                 console.log(response);
 
-                const p = $("<p></p>");
-
                 for (const i in response) {
-
+                    let p = $("<p></p>")
                     p.text(`${response[i].name}: ${response[i].body}`);
                     $(".formulario").append(p);
+                    console.log(response[i].name);
                 }
 
             }

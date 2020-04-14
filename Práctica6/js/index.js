dbfb832400e6e8bd73b1ce517d0e9e01b2e81b91
barbaragd <alu0101108452@ull.edu.es>
2020-04-14 10:16:57 +0100

    Arreglado fallo :), funciona con postId

--- Práctica6/js/index.js
+++ Práctica6/js/index.js
@@ -1,24 +1,17 @@
-const xhttp = new XMLHttpRequest();
-
 $(document).ready(function() {
     $(".boton").click(function() {
-
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                const response = JSON.parse(this.responseText);
-                console.log(response);
-
+        postId = document.querySelector("#id").value;
+        $.ajax({
+            type: "GET",
+            url: "https://jsonplaceholder.typicode.com/comments?",
+            data: `postId=${postId}`,
+            success: function(response) {
                 for (const i in response) {
-                    let p = $("<p></p>")
+                    let p = $("<p></p>");
                     p.text(`${response[i].name}: ${response[i].body}`);
-                    $(".formulario").append(p);
-                    console.log(response[i].name);
+                    $(".container").append(p);
                 }
-
-            }
-        };
-        xhttp.open("GET", "https://jsonplaceholder.typicode.com/comments?postId=1", true);
-        xhttp.send();
-
+            },
+        });
     });
 });

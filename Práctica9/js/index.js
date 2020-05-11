8237d14e0b3f01f3e8f809c90e893a79ca9bdef2
Damián Eduardo Domínguez <alu0101138183@ull.edu.es>
2020-05-12 00:48:58 +0100

    Reintentando :)

--- Práctica9/js/index.js
+++ Práctica9/js/index.js
@@ -1,10 +1,26 @@
-// import 'materialize'
-// // from './js/materialize.js's
-
-//Acordeon
 document.addEventListener("DOMContentLoaded", function () {
-    var elems = document.querySelectorAll(".collapsible");
-    var instances = M.Collapsible.init(elems);
+    var elemsCollapsible = document.querySelectorAll(".collapsible");
+    var instancesCollapsible = M.Collapsible.init(elemsCollapsible);
+
+    var elems = document.querySelectorAll(".carousel");
+    var instances = M.Carousel.init(elems);
+
+    const izquierda = document.querySelector("#izda");
+    const derecha = document.querySelector("#dcha");
+    let actual;
+    izquierda.onclick = () => {
+        actual = instances[0].el.querySelector(".active");
+        actual.setAttribute("tabindex", "-1");
+        actual.setAttribute("aria-hidden", "true");
+        instances[0].prev();
+    };
+
+    derecha.onclick = () => {
+        actual = instances[0].el.querySelector(".active");
+        actual.setAttribute("tabindex", "-1");
+        actual.setAttribute("aria-hidden", "true");
+        instances[0].next();
+    };
 });
 const boton1 = document.querySelector("#button-1");
 boton1.onclick = () => {
@@ -34,55 +50,4 @@ boton3.onclick = () => {
     } else {
         document.querySelector("#body-3").setAttribute("hidden", "false");
     }
-};
-//!Acordeon
-
-var elems;
-var instances;
-
-document.addEventListener("DOMContentLoaded", function () {
-    elems = document.querySelectorAll(".carousel");
-    instances = M.Carousel.init(elems);
-});
-const izquierda = document.querySelector("#izda");
-const derecha = document.querySelector("#dcha");
-const carr_items = document.querySelectorAll(".carousel-item");
-
-izquierda.onclick = () => {
-    // for (let index = 0; index < carr_items.length; index++) {
-    //     const element = carr_items[index];
-    //     const element = document.querySelector("#body-1").getAttribute("hidden");
-    // }
-    // carr_items.forEach(element => {
-    //     let id = element.getAttribute("id");
-    //     if (element.getAttribute("tabindex") == '0') {
-    //         element.setAttribute("tabindex", "-1");
-    //         element.setAttribute("aria-hidden", "true");
-    //     } else if (element.getAttribute("id") == id - 1) { // tener en cuenta que si es 0 hay que sumarle carr_items.lenght
-    //         element.setAttribute("tabindex", "0");
-    //         element.setAttribute("aria-hidden", "false");
-    //     }
-    // });
-
-    let current = instances[0].el.querySelector(".active");
-    current.setAttribute("tabindex", "-1");
-    current.setAttribute("aria-hidden", "true");
-    current.blur();
-    instances[0].prev();
-    let newEl = instances[0].el.querySelector(".active");
-    newEl.focus();
-    newEl.setAttribute("tabindex", "0");
-    newEl.setAttribute("aria-hidden", "true");
-};
-
-derecha.onclick = () => {
-    let current = instances[0].el.querySelector(".active");
-    current.setAttribute("tabindex", "-1");
-    current.setAttribute("aria-hidden", "true");
-    current.blur();
-    instances[0].next();
-    let newEl = instances[0].el.querySelector(".active");
-    newEl.focus();
-    newEl.setAttribute("tabindex", "0");
-    newEl.setAttribute("aria-hidden", "true");
 };

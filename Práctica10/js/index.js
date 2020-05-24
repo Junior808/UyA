297f10cfa31d906c2974176518e2b5d57aabf8c6
Damián Eduardo Domínguez <alu0101138183@ull.edu.es>
2020-05-24 18:50:45 +0100

    Inicio responsive y html sin estilos

--- Práctica10/js/index.js
+++ Práctica10/js/index.js
@@ -1,12 +1,8 @@
 //Menú barra de navegación
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems);
-});
+document.addEventListener("DOMContentLoaded", function () {
+    var elemsSideNav = document.querySelectorAll(".sidenav");
+    var instancesSideNav = M.Sidenav.init(elemsSideNav);
 
-var instance = M.Carousel.init({
-    // fullWidth: true,
-    indicators: true
+    var elemsParallax = document.querySelectorAll(".parallax");
+    var instancesParallax = M.Parallax.init(elemsParallax);
 });
